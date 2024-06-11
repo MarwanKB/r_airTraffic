7050825cce6b54417468c02b1179a976874bccfb
Marwan <marwan.boubchir@outlook.fr>
2024-06-11 15:10:31 +0200

    Création du Script de connexion à la bse

---
+++ scriptDb.R
@@ -0,0 +1,30 @@
+# Install the necessary packages
+install.packages("DBI")
+install.packages("RMySQL")
+
+# Load the packages
+library(DBI)
+library(RMySQL)
+
+# Define connection parameters
+host <- "mysql-traffic-aerien.alwaysdata.net"
+port <- 3306
+dbname <- "traffic-aerien_projet"
+user <- "363257_user"
+password <- "root_marwan"
+
+# Establish the connection
+con <- dbConnect(
+  RMySQL::MySQL(),
+  host = host,
+  port = port,
+  dbname = dbname,
+  user = user,
+  password = password
+)
+
+# Check the connection
+print(con)
+
+# Don't forget to disconnect when you're done
+# dbDisconnect(con)
